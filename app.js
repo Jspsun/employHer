b6cf5ce59f5dce1b3d0b3231cc6c8c08c5dfa509
RobertXu <RobertYC.Xu@gmail.com>
2016-11-27 02:24:53 -0500

    afdasdfa

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 console.log("hello");
-// var app = express();
+var app = express();
 
 // app.use(bodyParser.urlencoded({extended: false})); 
 // app.set('port', (process.env.PORT || 5000));

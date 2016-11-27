da4a2193ba935f8d309bf0cb628d3245174784c3
RobertXu <RobertYC.Xu@gmail.com>
2016-11-27 02:55:28 -0500

    asdfasdf

--- app.js
+++ app.js
@@ -3,27 +3,26 @@ var bodyParser = require('body-parser');
 console.log("hello");
 var app = express();
 
-// app.use(bodyParser.urlencoded({extended: false})); 
-// app.set('port', (process.env.PORT || 5000));
-// app.use(express.static(__dirname + '/public'));
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false})); 
 
-// app.get("/", function (request, response) {
-//   console.log("hello")
-// });
+app.set('views', __dirname + '/views');
 
 
-// app.post("/message", function (request, response) {
-//   console.log(request.body.Body);
-//   console.log(request.body.From);  
+app.get("/", function (request, response) {
+  response.render("views/index.html");
+});
+
+
+app.post("/message", function (request, response) {
+  console.log(request.body.Body);
+  console.log(request.body.From);  
   
-// });
+});
 
 
 
 // console.log("hello")
 
-// var listener = app.listen(process.env.PORT, function () {
-//   console.log('Your app is listening on port ' + listener.address().port);
-// });
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});

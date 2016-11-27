aa913822b8c9e98272e22c9c6c98ed62f5f072b9
RobertXu <RobertYC.Xu@gmail.com>
2016-11-27 02:12:03 -0500

    asdfadf

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ console.log("hello");
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false})); 
-
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get("/", function (request, response) {
   console.log("hello")
@@ -21,6 +24,6 @@ app.post("/message", function (request, response) {
 
 console.log("hello")
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });

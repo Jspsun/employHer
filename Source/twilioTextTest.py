fbee53061bc2095f7b5190a2ea724693024e75e4
Robert Xu <robert.xu@uwaterloo.ca>
2016-11-26 19:46:20 -0500

    added node.js files

--- Source/twilioTextTest.py
+++ Source/twilioTextTest.py
@@ -1,13 +1,16 @@
-from twilio.rest import TwilioRestClient
+from flask import Flask, request, redirect
+import twilio.twiml
 
-account_sid = "{{ AC9f4dc30b29b932a901627da4c92a38a4 }}" # Your Account SID from www.twilio.com/console
-auth_token  = "{{ 0c85242dcc5c50703f691d66fb1d931e }}"  # Your Auth Token from www.twilio.com/console
+app = Flask(__name__)
 
-client = TwilioRestClient(account_sid, auth_token)
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
 
-message = client.messages.create(body="Hello from Python",
-    to="+16479831888",    # Replace with your phone number
-    from_="+16475601733") # Replace with your Twilio number
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
 
-print(message.sid)
+if __name__ == "__main__":
+    app.run(debug=True)
 

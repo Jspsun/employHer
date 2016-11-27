705f5fe0d9b37827216af4e06b59138508bc6e01
Robert Xu <robert.xu@uwaterloo.ca>
2016-11-27 01:02:30 -0500

    Asdfa

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended: false})); 
 var port=Number(process.env.PORT || 3000);
+
+app.get("/", function (request, response) {
+  response.sendFile(/views/index.html);
+});
+
+
 app.post("/message", function (request, response) {
   console.log(request.body.Body);
   console.log(request.body.From);  
   
 });
 
-app.get("/", function (request, response) {
-  response.sendFile(/views/index.html);
-});
+
 
 console.log("hello")
 

49add7b632b544ef4aa9c2aa8e66d247d4199119
Robert Xu <robert.xu@uwaterloo.ca>
2016-11-26 23:24:09 -0500

    uhh

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ app.post("/message", function (request, response) {
   
 });
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+
+console.log("hello")
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);

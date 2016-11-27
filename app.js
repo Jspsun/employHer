7213b81e4a1d0b139c3c2a637e208abc3cbc25d1
RobertXu <RobertYC.Xu@gmail.com>
2016-11-27 01:11:52 -0500

    debug

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var app = express();
+// var app = express();
 
-app.use(bodyParser.urlencoded({extended: false})); 
-var port=Number(process.env.PORT || 3000);
+// app.use(bodyParser.urlencoded({extended: false})); 
+// var port=Number(process.env.PORT || 3000);
 
-app.get("/", function (request, response) {
-  console.log("hello")
-});
+// app.get("/", function (request, response) {
+//   console.log("hello")
+// });
 
 
-app.post("/message", function (request, response) {
-  console.log(request.body.Body);
-  console.log(request.body.From);  
+// app.post("/message", function (request, response) {
+//   console.log(request.body.Body);
+//   console.log(request.body.From);  
   
-});
+// });
 
 
 
-console.log("hello")
+// console.log("hello")
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });

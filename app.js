cd4981506a5177101a6d4e6df1221c008e47734b
RobertXu <RobertYC.Xu@gmail.com>
2016-11-27 02:01:54 -0500

    aadfa

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 console.log("hello");
-// var app = express();
+var app = express();
 
-// app.use(bodyParser.urlencoded({extended: false})); 
-// var port=Number(process.env.PORT || 3000);
-
-// app.get("/", function (request, response) {
-//   console.log("hello")
-// });
+app.use(bodyParser.urlencoded({extended: false})); 
 
 
-// app.post("/message", function (request, response) {
-//   console.log(request.body.Body);
-//   console.log(request.body.From);  
+app.get("/", function (request, response) {
+  console.log("hello")
+});
+
+
+app.post("/message", function (request, response) {
+  console.log(request.body.Body);
+  console.log(request.body.From);  
   
-// });
+});
 
 
 
-// console.log("hello")
+console.log("hello")
 
-// var listener = app.listen(process.env.PORT, function () {
-//   console.log('Your app is listening on port ' + listener.address().port);
-// });
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
